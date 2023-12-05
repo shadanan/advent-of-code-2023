907fa14c4223e0924e21d13d58f78c7610f444d4
Shad Sharma <shadanan@gmail.com>
2023-12-04 22:03:59 -0800

    Fix type hints and formatting in day3a+b (#3)

--- day3a.py
+++ day3a.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-from dataclasses import dataclass
 import re
+from dataclasses import dataclass
 
 NUMBER_PATERN = re.compile(r"\d+")
 
-with open("input3a.txt", "r") as fp:
+with open("input3.txt", "r") as fp:
     lines = fp.read().strip().splitlines()
+
+
 @dataclass
 class PartNumber:
     row: int
@@ -13,14 +15,18 @@ class PartNumber:
     end: int
     value: int
 
-def find_maybe_part_numbers(lines: list[str]) -> list[PartNumber]:
-    nums = []
+
+def find_part_numbers(lines: list[str]) -> list[PartNumber]:
+    part_numbers = []
     for row, line in enumerate(lines):
         for match in NUMBER_PATERN.finditer(line):
-            nums.append(PartNumber(row, match.start(), match.end(), int(match.group())))
-    return nums
+            pn = PartNumber(row, match.start(), match.end(), int(match.group()))
+            if is_valid(lines, pn):
+                part_numbers.append(pn)
+    return part_numbers
 
-def is_valid(lines, part_number: PartNumber) -> list[tuple[int, int]]:
+
+def is_valid(lines, part_number: PartNumber) -> bool:
     neighbors = [
         (part_number.row, part_number.start - 1),
         (part_number.row, part_number.end),
@@ -34,16 +40,13 @@ def is_valid(lines, part_number: PartNumber) -> list[tuple[int, int]]:
     for r, c in neighbors:
         if r >= 0 and r < len(lines) and c >= 0 and c < len(lines[0]):
             valid_neighbors.append((r, c))
-    
+
     # Check if neighbors contains a symbol
     for r, c in valid_neighbors:
         if not lines[r][c].isnumeric() and lines[r][c] != ".":
             return True
     return False
 
-if __name__ == '__main__':
-    total = 0
-    for maybe_part_number in find_maybe_part_numbers(lines):
-        if is_valid(lines, maybe_part_number):
-            total += maybe_part_number.value
-    print(total)
+
+if __name__ == "__main__":
+    print(sum([pn.value for pn in find_part_numbers(lines)]))
